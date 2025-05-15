a90850cb32d405e81d913a670e3c6becaf62cbde
Arnold <arnoldkorir201@gmail.com>
2025-05-15 09:51:03 +0300

    completed Anagram detector lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+class Anagram:
+
+    def __init__(self,word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self,items ):
+        matches =[]
+        for item in items: 
+            if item.lower() != self.word and sorted(item.lower()) == self.sorted_word:
+                matches.append(item)
+        return matches   
